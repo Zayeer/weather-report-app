43bd9e31a6f7f9049dc5f587b2da84339e5186e6
Shaik Zayeer Hussain <zayeer.zh@gmail.com>
2020-09-15 13:56:26 +0530

    Updates

---
+++ precache-manifest.e26ca982b5e3178aadd0fec2e8d40de8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6119bc6efe7f4241c83592cbb3dbc615",
+    "url": "/weather-report-app/index.html"
+  },
+  {
+    "revision": "61339b6bb8c0e533b871",
+    "url": "/weather-report-app/static/css/main.e425e8f7.chunk.css"
+  },
+  {
+    "revision": "3214325ae1bf85d1eb81",
+    "url": "/weather-report-app/static/js/2.cef3ac92.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/weather-report-app/static/js/2.cef3ac92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61339b6bb8c0e533b871",
+    "url": "/weather-report-app/static/js/main.ec03f085.chunk.js"
+  },
+  {
+    "revision": "9cc289cf5bb4689166aa",
+    "url": "/weather-report-app/static/js/runtime-main.68074230.js"
+  }
+]);
